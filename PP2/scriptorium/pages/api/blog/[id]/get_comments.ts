914969d2058229d29b4ede6ef6b01ff812c01f7f
YehyunLee <glad7cu@gmail.com>
2024-11-27 01:49:00 -0500

    Get comments type converted -- this one was hard

--- PP2/scriptorium/pages/api/blog/[id]/get_comments.ts
+++ PP2/scriptorium/pages/api/blog/[id]/get_comments.ts
@@ -1,5 +1,28 @@
 import prisma from '../../../../utils/prisma';
 import {verifyUser} from "../../../../utils/verify_user";
+import type {NextApiRequest, NextApiResponse} from 'next';
+
+// Add interfaces at the top of the file
+interface CommentRating {
+  rating: number;
+}
+
+interface CommentAuthor {
+  firstName: string;
+  lastName: string;
+  email: string;
+  avatarUrl: string | null;
+}
+
+interface Comment {
+  id: number;
+  content: string;
+  author: CommentAuthor;
+  ratings: CommentRating[];
+  parentCommentId: number | null;
+  totalRates?: number;
+  averageRate?: number;
+}
 
 /**
  * Blog API: Retrieve list of all comments for a blog post
@@ -8,7 +31,7 @@ import {verifyUser} from "../../../../utils/verify_user";
  * Access: Public (some hidden posts are shown to authors)
  * Payload: {page?, limit?}
  */
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'GET') {
         res.status(405).json({message: 'Method is not allowed'});
         return;
@@ -17,8 +40,8 @@ export default async function handler(req, res) {
     const {id} = req.query;
     const {page = 1, limit = 10} = req.query;
 
-    const pageNumber = parseInt(page);
-    const pageSize = parseInt(limit);
+    const pageNumber = parseInt(page as string);
+    const pageSize = parseInt(limit as string);
 
     const offset = (pageNumber - 1) * pageSize; // minus 1 because page starts from 1
 
@@ -27,7 +50,7 @@ export default async function handler(req, res) {
     try {
         // Build the search conditions
         const searchConditions = {
-            id: parseInt(id),
+            id: parseInt(id as string),
             OR: [
                 {
                     hidden: false,
@@ -74,13 +97,27 @@ export default async function handler(req, res) {
         const totalPages = Math.ceil(totalComments / pageSize);
         const paginatedComments = comments.slice(offset, offset + pageSize);
 
-        paginatedComments.forEach(comment => {
-            const totalRates = comment.ratings.length;
-            const averageRate = comment.ratings.reduce((acc, rating) => acc + rating.rate, 0) / totalRates;
-            comment.totalRates = totalRates;
-            comment.averageRate = averageRate;
-            delete comment.ratings;
+        // paginatedComments.forEach(comment => {
+        //     const totalRates = comment.ratings.length;
+        //     const averageRate = comment.ratings.reduce((acc, rating) => acc + rating.rate, 0) / totalRates;
+        //     comment.totalRates = totalRates;
+        //     comment.averageRate = averageRate;
+        //     delete comment.ratings;
+        // });
+
+
+        // Update the problematic section
+        paginatedComments.forEach((comment: Comment) => {
+        const totalRates = comment.ratings.length;
+        const averageRate = totalRates > 0 
+            ? comment.ratings.reduce((acc, curr) => acc + curr.rating, 0) / totalRates 
+            : 0;
+            
+        (comment as any).totalRates = totalRates;
+        (comment as any).averageRate = averageRate;
+        delete (comment as any).ratings;
         });
+
 
         res.status(200).json({
             comments: paginatedComments,
@@ -88,7 +125,7 @@ export default async function handler(req, res) {
             totalPages,
             totalComments
         });
-    } catch (error) {
+    } catch (error: any) {
         res.status(500).json({error: 'Internal Server Error', details: error.message});
     }
 }
